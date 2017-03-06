5dcde507c8b94964e68b821fa536eee4b1b32768
Christian <wyqer@wyqer.net>
2017-03-06 20:41:52 +0100

    Fixed BI Revive Error
    If you start the mission not from the editor or dedicated server, you've
    gotten an error message. So I moved the params directly into the list
    without include. Nice side effect is, that you can adjust the default
    values now without problems.

--- Missionframework/ui/liberation_titles.hpp
+++ Missionframework/ui/liberation_titles.hpp
@@ -33,7 +33,7 @@ class RscTitles
 		w = 0.4 * safezoneW;
 		y = 0.65 * safezoneH + safezoneY;
 		h = 0.1 * safezoneH;
-		text = "[GREUH] and the Killah Potatoes presents";
+		text = "[GREUH] and the Killah Potatoes present";
 		shadow = 1;
 	  };
 	  class GenericLabelShadow : GenericLabel2{
--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -102,7 +102,87 @@ class Params
 		texts[] = { "" };
 		default = "";
 	};
-	#include "\a3\Functions_F\Params\paramRevive.hpp"
+	class ReviveMode
+	{
+		title = $STR_A3_ReviveMode;
+		isGlobal = 1;
+		values[] = {-100,0,1};
+		texts[] = {$STR_A3_MissionDefault, $STR_A3_Disabled, $STR_A3_EnabledForAllPlayers};
+		default = 0;
+		function = "bis_fnc_paramReviveMode";
+	};
+	class ReviveDuration
+	{
+		title = $STR_A3_ReviveDuration;
+		isGlobal = 1;
+		values[] = {-100,6,8,10,12,15,20,25,30};
+		texts[] = {$STR_A3_MissionDefault,6,8,10,12,15,20,25,30};
+		default = 6;
+		function = "bis_fnc_paramReviveDuration";
+	};
+	class ReviveRequiredTrait
+	{
+		title = $STR_A3_RequiredTrait;
+		isGlobal = 1;
+		values[] = {-100,0,1};
+		texts[] = {$STR_A3_MissionDefault,$STR_A3_None,$STR_A3_Medic};
+		default = 1;
+		function = "bis_fnc_paramReviveRequiredTrait";
+	};
+	class ReviveMedicSpeedMultiplier
+	{
+		title = $STR_A3_RequiredTrait_MedicSpeedMultiplier;
+		isGlobal = 1;
+		values[] = {-100,1,1.5,2,2.5,3};
+		texts[] = {$STR_A3_MissionDefault,"1x","1.5x","2x","2.5x","3x"};
+		default = 1;
+		function = "bis_fnc_paramReviveMedicSpeedMultiplier";
+	};
+	class ReviveRequiredItems
+	{
+		title = $STR_A3_RequiredItems;
+		isGlobal = 1;
+		values[] = {-100,0,1,2};
+		texts[] = {$STR_A3_MissionDefault,$STR_A3_None,$STR_A3_Medikit,$STR_A3_FirstAidKitOrMedikit};
+		default = 2;
+		function = "bis_fnc_paramReviveRequiredItems";
+	};
+	class ReviveRequiredItemsFakConsumed
+	{
+		title = $STR_A3_RequiredItems_ConsumeFirstAidKit;
+		isGlobal = 1;
+		values[] = {-100,0,1};
+		texts[] = {$STR_A3_MissionDefault,$STR_lib_info_no,$STR_lib_info_yes};
+		default = 0;
+		function = "bis_fnc_paramReviveRequiredItemsFakConsumed";
+	};
+	class UnconsciousStateMode
+	{
+		title = $STR_A3_IncapacitationMode;
+		isGlobal = 1;
+		values[] = {-100,0,1,2};
+		texts[] = {$STR_A3_MissionDefault,$STR_A3_Basic,$STR_A3_Advanced,$STR_A3_Realistic};
+		default = 0;
+		function = "bis_fnc_paramReviveUnconsciousStateMode";
+	};
+	class ReviveBleedOutDuration
+	{
+		title = $STR_A3_BleedOutDuration;
+		isGlobal = 1;
+		values[] = {-100,10,15,20,30,45,60,90,180};
+		texts[] = {$STR_A3_MissionDefault,10,15,20,30,45,60,90,180};
+		default = 180;
+		function = "bis_fnc_paramReviveBleedOutDuration";
+	};
+	class ReviveForceRespawnDuration
+	{
+		title = $STR_A3_ForceRespawnDuration;
+		isGlobal = 1;
+		values[] = {-100,3,4,5,6,7,8,9,10};
+		texts[] = {$STR_A3_MissionDefault,3,4,5,6,7,8,9,10};
+		default = 10;
+		function = "bis_fnc_paramReviveForceRespawnDuration";
+	};
 	class Spacer2 {
 		title = "";
 		values[] = { "" };
