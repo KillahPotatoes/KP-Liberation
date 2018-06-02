93e58de1d7920c7420301aa7371081915c0d11f5
Wyqer <wyqer@wyqer.net>
2018-06-02 15:28:59 +0200

    Module structure and basic files for player options

---
+++ Missionframework/modules/99_playerOptions/functions.hpp
@@ -0,0 +1,12 @@
+/*
+    KP LIBERATION MODULE FUNCTIONS
+
+    File: functions.hpp
+    Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
+    Date: 2018-06-02
+    Last Update: 2018-06-02
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+    Defines for all functions, which are brought by this module.
+*/
