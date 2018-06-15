5c85e3145ccbb589bbf85cc075996575a88e6b38
Wyqer <wyqer@wyqer.net>
2018-06-15 22:40:27 +0200

    First Draft for the basic ui defines and classes

--- Missionframework/modules/00_init/ui.hpp
+++ Missionframework/modules/00_init/ui.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_ui.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2017-10-16
-    Last Update: 2018-01-05
+    Last Update: 2018-01-15
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -12,3 +12,4 @@
 */
 
 #include "ui\KPLIB_defines.hpp"
+#include "ui\KPLIB_baseclasses.hpp"
