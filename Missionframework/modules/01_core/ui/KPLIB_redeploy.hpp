14ce332df4a047c5ef46090f2fa1e952a2782b61
Wyqer <wyqer@wyqer.net>
2018-08-03 18:00:22 +0200

    Redeploy dialog adjustment

--- Missionframework/modules/01_core/ui/KPLIB_redeploy.hpp
+++ Missionframework/modules/01_core/ui/KPLIB_redeploy.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_redeploy.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2017-10-28
-    Last Update: 2018-07-27
+    Last Update: 2018-08-03
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -20,36 +20,33 @@ class KPLIB_redeploy {
     class controlsBackground {
 
         class KPLIB_DialogTitle: KP_DialogTitleC {
-            text = $STR_DEPLOY_TITLE;
+            text = "$STR_DEPLOY_TITLE";
         };
 
         class KPLIB_DialogArea: KP_DialogBackgroundC {};
 
-        class KPLIB_LabelLoadout: KP_Text {
-            text = $STR_DEPLOY_LOADOUTLIST;
+        class KPLIB_LabelLoadout: KP_InlineTitle {
+            text = "$STR_DEPLOY_LOADOUTLIST";
             x = KP_GETCX(KP_X_VAL_C,KP_WIDTH_VAL_C,0,1);
             y = KP_GETCY(KP_Y_VAL_C,KP_HEIGHT_VAL_C,0,16);
             w = KP_GETW(KP_WIDTH_VAL_C,1);
             h = KP_GETH(KP_HEIGHT_VAL_C,16);
-            sizeEx = KP_TEXT_L;
         };
 
-        class KPLIB_LabelDeploy: KP_Text {
-            text = $STR_DEPLOY_SPAWNLIST;
+        class KPLIB_LabelDeploy: KP_InlineTitle {
+            text = "$STR_DEPLOY_SPAWNLIST";
             x = KP_GETCX(KP_X_VAL_C,KP_WIDTH_VAL_C,0,1);
             y = KP_GETCY(KP_Y_VAL_C,KP_HEIGHT_VAL_C,2,16);
             w = KP_GETW(KP_WIDTH_VAL_C,1);
             h = KP_GETH(KP_HEIGHT_VAL_C,16);
-            sizeEx = KP_TEXT_L;
         };
 
-        class KPLIB_LabelMap: KP_Text {
-            text = $STR_DEPLOY_MAPVIEW;
+        class KPLIB_LabelMap: KP_InlineTitle {
+            text = "$STR_DEPLOY_MAPVIEW";
             x = KP_GETCX(KP_X_VAL_C,KP_WIDTH_VAL_C,0,1);
             y = KP_GETCY(KP_Y_VAL_C,KP_HEIGHT_VAL_C,7,16);
             w = KP_GETW(KP_WIDTH_VAL_C,1);
             h = KP_GETH(KP_HEIGHT_VAL_C,16);
-            sizeEx = KP_TEXT_L;
         };
     };
 
@@ -80,7 +77,7 @@ class KPLIB_redeploy {
         };
 
         class KPLIB_DeployButton: KP_DialogButtonC {
-            text = $STR_DEPLOY_BUTTON;
+            text = "$STR_DEPLOY_BUTTON";
             action = "KPLIB_dialog_deploy = 1";
         };
     };
