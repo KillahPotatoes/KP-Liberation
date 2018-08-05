538fba3408322f9c8968f4a62e9cdc3417838b90
Wyqer <wyqer@wyqer.net>
2018-08-05 11:03:10 +0200

    Last update state admin dialog

--- Missionframework/modules/99_adminMenu/ui/KPLIB_admin.hpp
+++ Missionframework/modules/99_adminMenu/ui/KPLIB_admin.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_admin.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-07-27
-    Last Update: 2018-08-02
+    Last Update: 2018-08-05
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
