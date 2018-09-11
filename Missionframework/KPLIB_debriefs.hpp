eefdd538262253b2eafc5df771d5b0b4cb7930c9
Wyqer <wyqer@wyqer.net>
2018-09-11 20:44:49 +0200

    Moved parent classes to description.ext

--- Missionframework/KPLIB_debriefs.hpp
+++ Missionframework/KPLIB_debriefs.hpp
@@ -1,18 +1,26 @@
-class CfgDebriefing
-{
-    class KPLIB_Win
-    {
-        title =	$STR_DEBRIEF_WINTITLE;
-        subtitle = "";
-        description = $STR_DEBRIEF_WINDESCR;
-        pictureBackground = "";
-    };
+/*
+    KP LIBERATION DEBRIEFINGS FILE
 
-    class KPLIB_ConfigError
-    {
-        title = $STR_DEBRIEF_ERRORTITLE;
-        subtitle = $STR_DEBRIEF_ERRORSUBTITLE;
-        description = $STR_DEBRIEF_ERRORDESCR;
-        pictureBackground = "";
-    };
+    File: KPLIB_debriefs.hpp
+    Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
+    Date: 2017-10-16
+    Last Update: 2018-09-11
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+    Debriefing defines for KP Liberation.
+*/
+
+class KPLIB_Win {
+    title =	$STR_DEBRIEF_WINTITLE;
+    subtitle = "";
+    description = $STR_DEBRIEF_WINDESCR;
+    pictureBackground = "";
+};
+
+class KPLIB_ConfigError {
+    title = $STR_DEBRIEF_ERRORTITLE;
+    subtitle = $STR_DEBRIEF_ERRORSUBTITLE;
+    description = $STR_DEBRIEF_ERRORDESCR;
+    pictureBackground = "";
 };
--- Missionframework/KPLIB_functions.hpp
+++ Missionframework/KPLIB_functions.hpp
@@ -4,20 +4,18 @@
     File: KPLIB_functions.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2017-10-16
-    Last Update: 2018-08-31
+    Last Update: 2018-09-11
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
     Fetches all functions from the modules.
 */
 
-class CfgFunctions {
-    class KPLIB {
-        // Include procedures and functions from each module
-        #include "modules\00_init\functions.hpp"
-        #include "modules\01_core\functions.hpp"
-        #include "modules\02_build\functions.hpp"
-        #include "modules\99_adm\functions.hpp"
-        #include "modules\99_plm\functions.hpp"
-    };
+class KPLIB {
+    // Include functions from each module accordingly to the load order
+    #include "modules\00_init\functions.hpp"
+    #include "modules\01_core\functions.hpp"
+    #include "modules\02_build\functions.hpp"
+    #include "modules\99_adm\functions.hpp"
+    #include "modules\99_plm\functions.hpp"
 };
