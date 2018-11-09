ff2fb8fe2a62a6f4ecc14ea78c781c74773fa145
Dubjunk <bam2601@gmail.com>
2018-11-09 22:42:24 +0100

    more changes to the "core" module header

--- Missionframework/modules/02_core/ui/KPLIB_titles.hpp
+++ Missionframework/modules/02_core/ui/KPLIB_titles.hpp
@@ -4,11 +4,11 @@
     File: KPLIB_defines.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2017-10-28
-    Last Update: 2018-09-25
+    Last Update: 2018-11-09
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
-    The rsc titles, sounds and the player status overlay are defined here.
+        The rsc titles, sounds and the player status overlay are defined here.
 */
 
 class CfgSounds
