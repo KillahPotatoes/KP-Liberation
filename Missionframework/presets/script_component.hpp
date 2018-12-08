c67c3052556a6335eceac776e3c713e30491230e
Wyqer <wyqer84@gmail.com>
2018-12-08 08:49:17 +0100

    Small typo

--- Missionframework/presets/script_component.hpp
+++ Missionframework/presets/script_component.hpp
@@ -4,7 +4,7 @@
     File: script_component.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-12-07
-    Last Update: 2018-12-07
+    Last Update: 2018-12-08
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -17,15 +17,15 @@
 
 // Gets the value of a KPLIB preset variable
 #define KPLIB_PRST_GETVAR(VARIABLE)\
-    missionnamespace getVariable [KPLIB_PRST_VAR(VARIABLE), nil]
+    missionNamespace getVariable [KPLIB_PRST_VAR(VARIABLE), nil]
 
 // Sets the value of a KPLIB preset variable
 #define KPLIB_PRST_SETVAR(VARIABLE,CLASS)\
-    missionnamespace setVariable [KPLIB_PRST_VAR(VARIABLE), CLASS]
+    missionNamespace setVariable [KPLIB_PRST_VAR(VARIABLE), CLASS]
 
 // Creates a KPLIB preset array
 #define KPLIB_PRST_CAT_BEGIN(ARRAYNAME)\
-    missionnamespace setVariable [KPLIB_PRST_VAR(ARRAYNAME), []];
+    missionNamespace setVariable [KPLIB_PRST_VAR(ARRAYNAME), []];
 
 // Adds a classname to a preset array with resource costs
 #define KPLIB_PRST_CAT_ADD(ARRAYNAME, CLASSNAMESTRING)\
