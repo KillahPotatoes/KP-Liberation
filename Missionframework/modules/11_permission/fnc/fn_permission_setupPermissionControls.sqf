af0ea4ada35e37cc9219be7fc600f3e634a3341b
Dubjunk <bam2601@gmail.com>
2018-12-17 20:23:51 +0100

    added new function to setup the permission controls

---
+++ Missionframework/modules/11_permission/fnc/fn_permission_setupPermissionControls.sqf
@@ -0,0 +1,18 @@
+/*
+    KPLIB_fnc_permission_setupPermissionControls
+
+    File: fn_permission_setupPermissionControls.sqf
+    Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
+    Date: 2018-12-17
+    Last Update: 2018-12-17
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+        Reads the player permissions and applies them to the dialog controls.
+
+    Parameter(s):
+        NONE
+
+    Returns:
+        Function reached the end [BOOL]
+*/
--- Missionframework/modules/11_permission/ui/KPLIB_permission.hpp
+++ Missionframework/modules/11_permission/ui/KPLIB_permission.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_permission.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-12-07
-    Last Update: 2018-12-15
+    Last Update: 2018-12-17
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -35,6 +35,7 @@ class KPLIB_permission {
             y = KP_GETCY(KP_Y_VAL_C,KP_HEIGHT_VAL_C,0,48);
             w = KP_GETW(KP_WIDTH_VAL_C,2);
             h = KP_GETH(KP_HEIGHT_VAL_C,1);
+            onLBSelChanged = "[] call KPLIB_fnc_permission_setupPermissionControls";
         };
 
         class KPLIB_DialogCross: KPGUI_PRE_DialogCrossC {};
