194199deda1558e91187296ef4744ecae53fa869
Dubjunk <bam2601@gmail.com>
2018-12-19 17:12:09 +0100

    changed save to reset button

--- Missionframework/modules/11_permission/ui/KPLIB_permission.hpp
+++ Missionframework/modules/11_permission/ui/KPLIB_permission.hpp
@@ -4,7 +4,7 @@
     File: KPLIB_permission.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-12-07
-    Last Update: 2018-12-17
+    Last Update: 2018-12-19
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
@@ -40,8 +40,8 @@ class KPLIB_permission {
 
         class KPLIB_DialogCross: KPGUI_PRE_DialogCrossC {};
 
-        class KPLIB_SaveButton: KPGUI_PRE_DialogButtonC {
-            text = "$STR_KPLIB_DIALOG_BUTTON_SAVE";
+        class KPLIB_ResetButton: KPGUI_PRE_DialogButtonC {
+            text = "$STR_KPLIB_DIALOG_BUTTON_RESET";
             onButtonClick = "";
         };
 
