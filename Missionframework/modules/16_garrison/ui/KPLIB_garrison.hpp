1f793cfdf9d7e0a7ae4a45dfe987abc753496ad6
Wyqer <wyqer84@gmail.com>
2019-03-24 17:26:40 +0100

    Garrison dialog foundation

---
+++ Missionframework/modules/16_garrison/ui/KPLIB_garrison.hpp
@@ -0,0 +1,28 @@
+/*
+    KP Liberation Garrison Dialog
+
+    File: KPLIB_garrison.hpp
+    Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
+    Date: 2019-03-19
+    Last Update: 2019-03-24
+    License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
+
+    Description:
+        Dialog for the commander to manage blufor sector garrisons.
+*/
+
+class KPLIB_garrison {
+    idd = 75804;
+    movingEnable = 0;
+
+    class controlsBackground {
+
+        class KPLIB_DialogTitle: KPGUI_PRE_DialogTitle {};
+
+        class KPLIB_DialogArea: KPGUI_PRE_DialogBackground {};
+    };
+
+    class controls {
+
+    };
+};
