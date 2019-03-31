b0c7edf033fdf9b0c41bce16f25b1830167736b3
Wyqer <wyqer84@gmail.com>
2019-03-31 10:20:33 +0200

    Strings for garrison dialog

--- Missionframework/modules/16_garrison/ui/KPLIB_garrison.hpp
+++ Missionframework/modules/16_garrison/ui/KPLIB_garrison.hpp
@@ -24,7 +24,7 @@ class KPLIB_garrison {
         class KPLIB_DialogArea: KPGUI_PRE_DialogBackground {};
 
         class KPLIB_GarrisonListTitle: KPGUI_PRE_InlineTitle {
-            text = "Garrisons";
+            text = "$STR_KPLIB_DIALOG_GARRISON_LISTHEADER";
             x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,0,1);
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,0,16);
             w = KP_GETW(KP_WIDTH_VAL,4);
@@ -32,7 +32,7 @@ class KPLIB_garrison {
         };
 
         class KPLIB_GarrisonControlsTitle: KPGUI_PRE_InlineTitle {
-            text = "Add Units";
+            text = "$STR_KPLIB_DIALOG_GARRISON_REINFORCEHEADER";
             x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,3,4);
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,0,16);
             w = KP_GETW(KP_WIDTH_VAL,4);
@@ -52,53 +52,63 @@ class KPLIB_garrison {
 
         class KPLIB_GarrisonSectorName: KPGUI_PRE_InlineTitle {
             idc = 758042;
+            text = "";
             x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,1,4);
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,0,16);
             w = KP_GETW(KP_WIDTH_VAL,2);
             h = KP_GETH(KP_HEIGHT_VAL,16);
         };
 
-        class KPLIB_GarrisonInfantryLabel: KPGUI_PRE_Text {
+        class KPLIB_GarrisonAlertNote: KPLIB_GarrisonSectorName {
             idc = 758043;
-            text = "Infantry:";
-            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,1,4);
+            text = "$STR_KPLIB_DIALOG_GARRISON_ALERTNOTE";
+            colorBackground[] = {0.8, 0, 0, 0.4};
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,1,16);
-            w = KP_GETW(KP_WIDTH_VAL,8);
+        };
+
+        class KPLIB_GarrisonInfantryLabel: KPGUI_PRE_Text {
+            idc = 758044;
+            text = "$STR_KPLIB_DIALOG_GARRISON_INFLABEL";
+            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,1,4);
+            y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,2,16);
+            w = KP_GETW(KP_WIDTH_VAL,(16/3));
             h = KP_GETH(KP_HEIGHT_VAL,16);
         };
 
         class KPLIB_GarrisonInfantryAmount: KPLIB_GarrisonInfantryLabel {
-            idc = 758044;
+            idc = 758045;
             text = "";
-            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,3,8);
+            style = 1;
+            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,7,16);
+            w = KP_GETW(KP_WIDTH_VAL,16);
         };
 
         class KPLIB_GarrisonLightLabel: KPLIB_GarrisonInfantryLabel {
-            idc = 758045;
-            text = "Light Vehicles:";
-            y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,2,16);
-        };
-
-        class KPLIB_GarrisonLightAmount: KPLIB_GarrisonLightLabel {
             idc = 758046;
-            text = "";
-            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,3,8);
-        };
-
-        class KPLIB_GarrisonHeavyLabel: KPLIB_GarrisonInfantryLabel {
-            idc = 758047;
-            text = "Heavy Vehicles:";
+            text = "$STR_KPLIB_DIALOG_GARRISON_LIGHTLABEL";
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,3,16);
         };
 
-        class KPLIB_GarrisonHeavyAmount: KPLIB_GarrisonHeavyLabel {
-            idc = 758048;
+        class KPLIB_GarrisonLightAmount: KPLIB_GarrisonInfantryAmount {
+            idc = 758047;
             text = "";
-            x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,3,8);
+            y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,3,16);
+        };
+
+        class KPLIB_GarrisonHeavyLabel: KPLIB_GarrisonInfantryLabel {
+            idc = 758048;
+            text = "$STR_KPLIB_DIALOG_GARRISON_HEAVYLABEL";
+            y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,4,16);
+        };
+
+        class KPLIB_GarrisonHeavyAmount: KPLIB_GarrisonInfantryAmount {
+            idc = 758049;
+            text = "";
+            y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,4,16);
         };
 
         class KPLIB_GarrisonMap: KPGUI_PRE_MapControl {
-            idc = 758049;
+            idc = 7580410;
             x = KP_GETCX(KP_X_VAL,KP_WIDTH_VAL,1,4);
             y = KP_GETCY(KP_Y_VAL,KP_HEIGHT_VAL,1,2);
             w = KP_GETW(KP_WIDTH_VAL,2);
