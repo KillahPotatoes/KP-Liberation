4fabb97bd6b29658eaa1f23ac79af84e9213dec1
Wyqer <wyqer84@gmail.com>
2019-03-31 11:45:38 +0200

    garrison ui defines

--- Missionframework/modules/16_garrison/ui.hpp
+++ Missionframework/modules/16_garrison/ui.hpp
@@ -4,11 +4,12 @@
     File: ui.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-10-18
-    Last Update: 2019-03-24
+    Last Update: 2019-03-31
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
         Initializes the ui defines, dialogs and elements which are brought by this module.
 */
 
+#include "ui\defines.hpp"
 #include "ui\KPLIB_garrison.hpp"
