2fce7c851fcd1e5cb7725cf44e50233eefa6007d
Dubjunk <31448659+Dubjunk@users.noreply.github.com>
2019-04-11 19:52:42 +0200

    typo

--- Missionframework/modules/11_permission/fnc/script_component.hpp
+++ Missionframework/modules/11_permission/fnc/script_component.hpp
@@ -3,11 +3,11 @@
     File: script_component.hpp
     Author: KP Liberation Dev Team - https://github.com/KillahPotatoes
     Date: 2018-12-29
-    Last Update: 2018-12-29
+    Last Update: 2019-04-11
     License: GNU General Public License v3.0 - https://www.gnu.org/licenses/gpl-3.0.html
 
     Description:
-        Defines for build module scripts
+        Defines for permission module scripts
 */
 
 #define KPLIB_COLOR_RED    [0.9, 0, 0, 1]
