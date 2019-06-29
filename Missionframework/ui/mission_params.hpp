d676bf996e23fc7a147bc4a5bae951a4e8955bf5
Wyqer <wyqer84@gmail.com>
2019-06-29 15:02:41 +0200

    Closes #629

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -106,7 +106,7 @@ class Params
 		title = $STR_PARAMS_ARSENAL;
 		values[] = {0, 1};
 		texts[] = {$STR_PARAMS_ARSENAL_BI, $STR_PARAMS_ARSENAL_ACE};
-		default = "0";
+		default = 0;
 	};
 	class Spacer1 {
 		title = "";
