a17c35c87f564d435f8568b4f1235439b196feaf
Filip Maciejewski <veteran29q@gmail.com>
2020-01-28 19:34:39 +0100

    Implement automatic steam uploading

--- tools/buildtool/gulpfile.ts
+++ tools/buildtool/gulpfile.ts
@@ -7,7 +7,7 @@ import * as del from "del";
 
 import { resolve } from "path";
 
-import { MissionPaths } from "./src";
+import { MissionPaths, uploadLegacy } from "./src";
 import { Preset, FolderStructureInfo } from "./src";
 
 const ROOT_DIR = resolve('..', '..');
@@ -30,6 +30,7 @@ const paths: FolderStructureInfo = {
 let taskNames: string[] = [];
 let taskNamesPbo: string[] = [];
 let taskNamesZip: string[] = [];
+let taskNamesWorkshop: string[] = [];
 
 for (let preset of presets) {
     const mission = new MissionPaths(preset, paths);
@@ -115,6 +116,17 @@ for (let preset of presets) {
             .pipe(gulp.dest(mission.getWorkDir()))
     });
 
+    if (!!preset.workshopId) {
+
+        taskNamesWorkshop.push('workshop_' + taskName);
+
+        gulp.task('workshop_' + taskName, async () => {
+            const pboPath = resolve(mission.getWorkDir(), 'pbo', mission.getFullName() + '.pbo');
+            console.log(pboPath);
+
+            await uploadLegacy(preset.workshopId, pboPath);
+        });
+    }
 
 }
 
@@ -129,6 +141,8 @@ gulp.task('build', gulp.series(taskNames));
 gulp.task('pbo', gulp.series(taskNamesPbo));
 
 gulp.task('zip', gulp.series(taskNamesZip));
+
+gulp.task('workshop', gulp.series(taskNamesWorkshop));
 
 gulp.task('default',
     gulp.series(
--- tools/buildtool/src/Config.ts
+++ tools/buildtool/src/Config.ts
@@ -1,33 +1,38 @@
 export interface Preset {
-    /** 
+    /**
      * Path to folder with mission.sqm relative to "missionsFolder".
      * If mission.sqm is in root of "missionsFolder" should be empty string.
-     * 
+     *
      * @see FolderStructureInfo.missionsFolder
     */
     readonly sourceFolder: string;
 
-    /** 
-     * Path to file with mission configuration. 
+    /**
+     * Path to file with mission configuration.
      * Replacement of variables will be applied here.
     */
     readonly configFile: string;
 
-    /** 
+    /**
      * Name of mission (part before mapname)
     */
     readonly missionName: string;
 
-    /** 
+    /**
      * Map name
     */
     readonly map: string;
 
-    /** 
+    /**
      * key=>val of values to replace in config file
      * @see {VariablesReplacements}
     */
     readonly variables: VariablesReplacements;
+
+    /**
+     * Steam Workshop Id
+     */
+    readonly workshopId: string;
 }
 
 export interface VariablesReplacements {
@@ -36,21 +41,21 @@ export interface VariablesReplacements {
 }
 
 export interface FolderStructureInfo {
-    /** 
+    /**
      * Folder of folders with mission.sqm.
      * Value of "sourceFolder" from Preset will be appended to this path.
-     * 
+     *
      * @see {Preset}
      */
     readonly missionsFolder: string;
 
-    /** 
+    /**
      * Path to folder with mission framework files.
      */
     readonly frameworkFolder: string;
-    
-    /** 
-     * Directory containing built missions 
+
+    /**
+     * Directory containing built missions
      */
     readonly workDir: string;
 }
