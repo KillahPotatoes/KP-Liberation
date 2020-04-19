244235c3afb537e071832f51e63c331177012d8f
Wyqer <wyqer84@gmail.com>
2020-04-19 20:00:24 +0200

    Space after `,` in mission_params.hpp

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -20,8 +20,8 @@ class Params
     };
     class Unitcap {
         title = $STR_PARAMS_UNITCAP;
-        values[] = {0,1,2,3,4,5};
-        texts[] = {$STR_PARAMS_UNITCAP1,$STR_PARAMS_UNITCAP2,$STR_PARAMS_UNITCAP3,$STR_PARAMS_UNITCAP4,$STR_PARAMS_UNITCAP5,$STR_PARAMS_UNITCAP6};
+        values[] = {0, 1, 2, 3, 4, 5};
+        texts[] = {$STR_PARAMS_UNITCAP1, $STR_PARAMS_UNITCAP2, $STR_PARAMS_UNITCAP3, $STR_PARAMS_UNITCAP4, $STR_PARAMS_UNITCAP5, $STR_PARAMS_UNITCAP6};
         default = 2;
     };
     class Difficulty {
@@ -32,20 +32,20 @@ class Params
     };
     class Aggressivity {
         title = $STR_AGGRESSIVITY_PARAM;
-        values[] = {0,1,2,3,4};
-        texts[] = {$STR_AGGRESSIVITY_PARAM0, $STR_AGGRESSIVITY_PARAM1,$STR_AGGRESSIVITY_PARAM2,$STR_AGGRESSIVITY_PARAM3,$STR_AGGRESSIVITY_PARAM4};
+        values[] = {0, 1, 2, 3, 4};
+        texts[] = {$STR_AGGRESSIVITY_PARAM0, $STR_AGGRESSIVITY_PARAM1, $STR_AGGRESSIVITY_PARAM2, $STR_AGGRESSIVITY_PARAM3, $STR_AGGRESSIVITY_PARAM4};
         default = 2;
     };
     class AdaptToPlayercount {
         title = $STR_PARAM_ADAPT_TO_PLAYERCOUNT;
-        values[] = {1,0};
-        texts[] = {$STR_PARAMS_ENABLED,$STR_PARAMS_DISABLED};
+        values[] = {1, 0};
+        texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
     class Civilians {
         title = $STR_PARAMS_CIVILIANS;
-        values[] = {0,1,2,3};
-        texts[] = {$STR_PARAMS_CIVILIANS1,$STR_PARAMS_CIVILIANS2,$STR_PARAMS_CIVILIANS3,$STR_PARAMS_CIVILIANS4};
+        values[] = {0, 1, 2, 3};
+        texts[] = {$STR_PARAMS_CIVILIANS1, $STR_PARAMS_CIVILIANS2, $STR_PARAMS_CIVILIANS3, $STR_PARAMS_CIVILIANS4};
         default = 2;
     };
     class FirstFob {
@@ -62,25 +62,25 @@ class Params
     };
     class MaximumFobs {
         title = $STR_PARAM_FOBS_COUNT;
-        values[] = {3,5,7,10,15,20,26};
-        texts[] = {3,5,7,10,15,20,26};
+        values[] = {3, 5, 7, 10, 15, 20, 26};
+        texts[] = {3, 5, 7, 10, 15, 20, 26};
         default = 10;
     };
     class MaxSquadSize {
         title = $STR_PARAM_SQUAD_SIZE;
-        values[] = {0,1,2,4,6,8,10,12,16,20,24,30,36};
-        texts[] = {0,1,2,4,6,8,10,12,16,20,24,30,36};
+        values[] = {0, 1, 2, 4, 6, 8, 10, 12, 16, 20, 24, 30, 36};
+        texts[] = {0, 1, 2, 4, 6, 8, 10, 12, 16, 20, 24, 30, 36};
         default = 12;
     };
     class BluforDefenders {
         title = $STR_PARAM_BLUFOR_DEFENDERS;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
     class Autodanger {
         title = $STR_PARAM_AUTODANGER;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
@@ -92,13 +92,13 @@ class Params
     };
     class ShorterNights {
         title = $STR_SHORTER_NIGHTS_PARAM;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 0;
     };
     class Weather {
         title = $STR_WEATHER_PARAM;
-        values[] = {1,2,3};
+        values[] = {1, 2, 3};
         texts[] = {$STR_WEATHER_PARAM1, $STR_WEATHER_PARAM2, $STR_WEATHER_PARAM3};
         default = 3;
     };
@@ -146,50 +146,50 @@ class Params
     };
     class ReviveMode {
         title = $STR_A3_ReviveMode;
-        values[] = {0,1};
+        values[] = {0, 1};
         texts[] = {$STR_A3_Disabled, $STR_A3_EnabledForAllPlayers};
         default = 1;
     };
     class ReviveDuration {
         title = $STR_A3_ReviveDuration;
-        values[] = {6,8,10,12,15,20,25,30};
-        texts[] = {6,8,10,12,15,20,25,30};
+        values[] = {6, 8, 10,12, 15, 20, 25, 30};
+        texts[] = {6, 8, 10, 12, 15, 20, 25, 30};
         default = 6;
     };
     class ReviveRequiredTrait {
         title = $STR_A3_RequiredTrait;
-        values[] = {0,1};
-        texts[] = {$STR_A3_None,$STR_A3_Medic};
+        values[] = {0, 1};
+        texts[] = {$STR_A3_None, $STR_A3_Medic};
         default = 1;
     };
     class ReviveMedicSpeedMultiplier {
         title = $STR_A3_RequiredTrait_MedicSpeedMultiplier;
-        values[] = {1,1.5,2,2.5,3};
-        texts[] = {"1x","1.5x","2x","2.5x","3x"};
+        values[] = {1, 1.5, 2, 2.5, 3};
+        texts[] = {"1x", "1.5x", "2x", "2.5x", "3x"};
         default = 1;
     };
     class ReviveRequiredItems {
         title = $STR_A3_RequiredItems;
-        values[] = {0,1,2};
-        texts[] = {$STR_A3_None,$STR_A3_Medikit,$STR_A3_FirstAidKitOrMedikit};
+        values[] = {0, 1, 2};
+        texts[] = {$STR_A3_None, $STR_A3_Medikit, $STR_A3_FirstAidKitOrMedikit};
         default = 1;
     };
     class UnconsciousStateMode {
         title = $STR_A3_IncapacitationMode;
-        values[] = {0,1,2};
-        texts[] = {$STR_A3_Basic,$STR_A3_Advanced,$STR_A3_Realistic};
+        values[] = {0, 1, 2};
+        texts[] = {$STR_A3_Basic, $STR_A3_Advanced, $STR_A3_Realistic};
         default = 0;
     };
     class ReviveBleedOutDuration {
         title = $STR_A3_BleedOutDuration;
-        values[] = {10,15,20,30,45,60,90,180};
-        texts[] = {10,15,20,30,45,60,90,180};
+        values[] = {10, 15, 20, 30, 45, 60, 90, 180};
+        texts[] = {10, 15, 20, 30, 45, 60, 90, 180};
         default = 180;
     };
     class ReviveForceRespawnDuration {
         title = $STR_A3_ForceRespawnDuration;
-        values[] = {3,4,5,6,7,8,9,10};
-        texts[] = {3,4,5,6,7,8,9,10};
+        values[] = {3, 4, 5, 6, 7, 8, 9, 10};
+        texts[] = {3, 4, 5, 6, 7, 8, 9, 10};
         default = 10;
     };
     class Spacer2 {
@@ -248,25 +248,25 @@ class Params
     };
     class TeamkillPenalty {
         title = $STR_PARAM_TEAMKILL_PENALTY;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 0;
     };
     class CR_Building {
         title = $STR_PARAM_CR_BUILDING;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAM_CR_DAMAGED, $STR_PARAM_CR_DESTROYED};
         default = 0;
     };
     class HaloJump {
         title = $STR_HALO_PARAM;
-        values[] = {1,5,10,15,20,30,0};
+        values[] = {1, 5, 10, 15, 20, 30, 0};
         texts[] = {$STR_HALO_PARAM1, $STR_HALO_PARAM2, $STR_HALO_PARAM3, $STR_HALO_PARAM4, $STR_HALO_PARAM5, $STR_HALO_PARAM6, $STR_PARAMS_DISABLED};
         default = 0;
     };
     class ClearCargo {
         title = $STR_PARAM_CLEAR_CARGO;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
@@ -284,13 +284,13 @@ class Params
     };
     class LimitedZeus {
         title = $STR_PARAM_LIMITEDZEUS;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
     class ZeusAddEnemies {
         title = $STR_PARAM_ZEUSADDENEMIES;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 0;
     };
@@ -308,13 +308,13 @@ class Params
     };
     class Permissions {
         title = $STR_PERMISSIONS_PARAM;
-        values[] = {1,0};
+        values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
         default = 1;
     };
     class CleanupVehicles {
         title = $STR_CLEANUP_PARAM;
-        values[] = {0,1,2,4};
+        values[] = {0, 1, 2, 4};
         texts[] = {$STR_PARAMS_DISABLED, $STR_CLEANUP_PARAM1, $STR_CLEANUP_PARAM2, $STR_CLEANUP_PARAM3};
         default = 2;
     };
@@ -338,20 +338,20 @@ class Params
     };
     class ServerRestart {
         title = $STR_RESTART_PARAM;
-        values[] = {0,1,2,3,4,5,6};
-        texts[] = {$STR_PARAMS_DISABLED,"1","2","3","4","5","6"};
+        values[] = {0, 1, 2, 3, 4, 5, 6};
+        texts[] = {$STR_PARAMS_DISABLED, "1", "2", "3", "4", "5", "6"};
         default = 0;
     };
     class WipeSave1 {
         title = $STR_WIPE_TITLE;
-        values[] = {0,1};
-        texts[] =  {$STR_WIPE_NO,$STR_WIPE_YES};
+        values[] = {0, 1};
+        texts[] =  {$STR_WIPE_NO, $STR_WIPE_YES};
         default = 0;
     };
     class WipeSave2 {
         title = $STR_WIPE_TITLE_2;
-        values[] = {0,1};
-        texts[] = {$STR_WIPE_NO,$STR_WIPE_YES};
+        values[] = {0, 1};
+        texts[] = {$STR_WIPE_NO, $STR_WIPE_YES};
         default = 0;
     };
     class Spacer4 {
