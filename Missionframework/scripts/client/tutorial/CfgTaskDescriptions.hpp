5ac32b24f022cc651ddcc2bf489130d3f0817975
Wyqer <wyqer84@gmail.com>
2020-05-04 23:32:38 +0200

    Tutorial task descriptions config

---
+++ Missionframework/scripts/client/tutorial/CfgTaskDescriptions.hpp
@@ -0,0 +1,20 @@
+class KPLIB_Tasks_Tutorial_Main {
+    title = $STR_TUTORIALFSM_TASK_MAIN_TITLE;
+    description = $STR_TUTORIALFSM_TASK_MAIN_DESC;
+};
+class KPLIB_Tasks_Tutorial_Fob {
+    title = $STR_TUTORIALFSM_TASK_FOB_TITLE;
+    description = $STR_TUTORIALFSM_TASK_FOB_DESC;
+};
+class KPLIB_Tasks_Tutorial_Fob_01a {
+    title = $STR_TUTORIALFSM_TASK_FOB1A_TITLE;
+    description = $STR_TUTORIALFSM_TASK_FOB1A_DESC;
+};
+class KPLIB_Tasks_Tutorial_Fob_01b {
+    title = $STR_TUTORIALFSM_TASK_FOB1B_TITLE;
+    description = $STR_TUTORIALFSM_TASK_FOB1B_DESC;
+};
+class KPLIB_Tasks_Tutorial_Fob_02 {
+    title = $STR_TUTORIALFSM_TASK_FOB2_TITLE;
+    description = $STR_TUTORIALFSM_TASK_FOB2_DESC;
+};
