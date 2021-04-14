7ec035a03a4f2df1a1d07b97946eae46a037710e
befubo <befubo@gmx.ch>
2021-04-14 12:00:55 +0200

    increase unitcap

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -21,7 +21,7 @@ class Params {
         title = $STR_PARAMS_UNITCAP;
         values[] = {0, 1, 2, 3, 4, 5};
         texts[] = {$STR_PARAMS_UNITCAP1, $STR_PARAMS_UNITCAP2, $STR_PARAMS_UNITCAP3, $STR_PARAMS_UNITCAP4, $STR_PARAMS_UNITCAP5, $STR_PARAMS_UNITCAP6};
-        default = 3;
+        default = 4;
     };
     class Difficulty {
         title = $STR_PARAMS_DIFFICULTY;
